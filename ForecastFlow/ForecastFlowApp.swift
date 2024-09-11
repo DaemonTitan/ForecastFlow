145fc046e11c80e454c144f1f1474d10e7a7356d
DaemonTitan <79576307+DaemonTitan@users.noreply.github.com>
2024-09-11 21:54:50 +1000

    SetupProject

---
+++ ForecastFlow/ForecastFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastFlowApp.swift
+//  ForecastFlow
+//
+//  Created by Tony Chen on 11/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct ForecastFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
