743eaa91e684559d4697be5c1f5d1c7fe3280d30
DaemonTitan <79576307+DaemonTitan@users.noreply.github.com>
2024-09-30 15:54:44 +1000

    RefactorAPICall

--- ForecastFlow/Services/WeatherDataServices.swift
+++ ForecastFlow/Services/WeatherDataServices.swift
@@ -11,19 +11,14 @@ class WeatherDataServices {
     
     var currentWeatherData: CurrentWeatherModel? = nil
     
-    func decodeCurrentWeatherData(lat: Double, lon: Double, units: String) async throws -> CurrentWeatherModel? {
+    func decodeCurrentWeatherData(lat: Double, lon: Double, units: String, apiKey: String) async throws -> CurrentWeatherModel? {
         do {
             guard let url = URLs.getCurrentWeatherURL(lat: lat, lon: lon, apikey: apiKey, units: units) else { return nil }
-            
             let data = try await NetwordManager.downloadWeatherData(from: url)
-            print(data)
-            
             self.currentWeatherData = try JSONDecoder().decode(CurrentWeatherModel.self, from: data)
-            
         } catch {
             print("Decode Error appears")
         }
-        
         return currentWeatherData
     }
 }
